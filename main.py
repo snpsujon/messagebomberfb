60db4050ff0e0f5db8508332bdd0369c68765d37
SNP Sujon <43173562+snpsujon@users.noreply.github.com>
2020-11-22 21:20:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #visit : https://snpsujon.me
 #github : https://github.com/snpsujon
 
-
+#Firstly you need to install pyautogui type following command pip install pyautogui
 
 import pyautogui
 import time
@@ -12,7 +12,7 @@ limit = input("How many message you want to send : ")
 message = raw_input("Your Message: ")
 
 while limit > 0:
-    time.sleep(2)
-    pyautogui.typewrite( message)
-    pyautogui.press('enter')
-    limit = limit - 1
+    time.sleep(2) #For Time interval
+    pyautogui.typewrite(message)  #Your message 
+    pyautogui.press('enter')  #auto press from keyboard
+    limit = limit - 1  #loop decriment
